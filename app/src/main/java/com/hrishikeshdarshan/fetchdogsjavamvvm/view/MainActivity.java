b56fa275e79e0322440f4ebf1ef3ed9af126a1f6
Hrishikesh Darshan <hrishikesh7165@gmail.com>
2020-10-14 16:53:19 -0400

    Added DetailFragment.java and ListFragment

--- app/src/main/java/com/hrishikeshdarshan/fetchdogsjavamvvm/view/MainActivity.java
+++ app/src/main/java/com/hrishikeshdarshan/fetchdogsjavamvvm/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.hrishikeshdarshan.fetchdogsjavamvvm;
+package com.hrishikeshdarshan.fetchdogsjavamvvm.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.hrishikeshdarshan.fetchdogsjavamvvm.R;
 
 public class MainActivity extends AppCompatActivity {
 
